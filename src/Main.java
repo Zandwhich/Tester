9917fa1feef7f3753979aa3a3809b1d1887e2c8a
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2018-11-21 19:00:56 -0500

    Setup Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
-
+/**
+ * The Main class. Sets off the program.
+ *
+ *
+ * This project is meant to be a testing ground for ideas in different projects.
+ * I.E. if I want to implement a new tech in a project, but what to get some practice with using it first,
+ * this project is what can be used to play around with ideas and see exactly how things work, without worrying
+ * about messing the main project up.
+ *
+ * This project won't be as thoroughly commented, and most of the comments will mostly be made for myself as notes,
+ * rather than as comments meant for other people.
+ */
 public class Main {
-}
+
+    public static void main(String[] args) {
+
+    }//end main()
+
+}//end class Main
